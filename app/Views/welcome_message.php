3f2c688ed16a4e68e9f64cda0cece5b22f3e9c93
Wei-Wen Wu <1888atch25@gmail.com>
2024-07-30 21:31:07 +0800

    fix(images): solved 'Classification Query' cannot be displayed
    subtitle10.jpg

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,7 +225,7 @@ function MM_preloadImages() { //v3.0
           
                <table border="0" cellpadding="0" cellspacing="0">
       	<tr>
-      		<td align="right"><img src="images/subtitle01.jpg" border="0"></td>      	  
+      		<td align="right"><img src="images/subtitle10.jpg" border="0"></td>      	  
       	</tr>
       </table>    
       <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0">
