e03951d2db03b3451fbc795842b8a133a55aeafb
WeiYou0101 <ekko113019@gmail.com>
2024-08-02 20:34:41 +0800

    Style : rename school and department abbreviations

--- app/Views/register/sign_up_information.php
+++ app/Views/register/sign_up_information.php
@@ -45,17 +45,17 @@
                             <option value="" disabled selected>請選擇學校</option>
                             <option value="CCU">中正大學</option>
                             <option value="NCU">中央大學</option>
-                            <option value="school3">中山大學</option>
-                            <option value="school3">中興大學</option>
-                            <option value="school3">台灣大學</option>
-                            <option value="school3">清華大學</option>
-                            <option value="school3">交通大學</option>
-                            <option value="school3">嘉義大學</option>
-                            <option value="school3">宜蘭大學</option>
-                            <option value="school3">中原大學</option>
-                            <option value="school3">元智大學</option>
-                            <option value="school3">淡江大學</option>
-                            <option value="school3">逢甲大學</option>
+                            <option value="NSYSU">中山大學</option>
+                            <option value="NCHU">中興大學</option>
+                            <option value="NTU">台灣大學</option>
+                            <option value="NTHU">清華大學</option>
+                            <option value="NCTU">交通大學</option>
+                            <option value="NCYU">嘉義大學</option>
+                            <option value="NCKU">成功大學</option>
+                            <option value="CYCU">中原大學</option>
+                            <option value="YZU">元智大學</option>
+                            <option value="TKU">淡江大學</option>
+                            <option value="FCU">逢甲大學</option>
                         </select>
                     </td>
                 </tr>
@@ -66,16 +66,16 @@
                             <option value="" disabled selected>請選擇科系</option>
                             <option value="EE">電機系</option>
                             <option value="CS">資工系</option>
-                            <option value="department3">醫學系</option>
-                            <option value="department3">機械系</option>
-                            <option value="department3">資管系</option>
-                            <option value="department3">土木系</option>
-                            <option value="department3">地理系</option>
-                            <option value="department3">政治系</option>
-                            <option value="department3">數學系</option>
-                            <option value="department3">生命科學系</option>
-                            <option value="department3">光電系</option>
-                            <option value="department3">歷史系</option>
+                            <option value="Med">醫學系</option>
+                            <option value="ME">機械系</option>
+                            <option value="IM">資管系</option>
+                            <option value="CIE">土木系</option>
+                            <option value="department7">地理系</option>
+                            <option value="PS">政治系</option>
+                            <option value="MATH">數學系</option>
+                            <option value="LS">生命科學系</option>
+                            <option value="DoP">光電系</option>
+                            <option value="Hist">歷史系</option>
                             <!-- Add more options as needed -->
                         </select>
                     </td>
