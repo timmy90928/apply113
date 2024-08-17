ed72c100443567f9c25f8de6497300bed055593d
WeiYou0101 <ekko113019@gmail.com>
2024-08-17 17:03:43 +0800

    feat : personal_page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,5 @@ $routes->get('/home/sign_up_information', 'Home::sign_up_information');
 $routes->get('/home/sign_up_school', 'Home::sign_up_school');
 $routes->get('/home/sign_up_system_mainpage', 'Home::sign_up_system_mainpage');
 $routes->get('/home/wishlist', 'Home::wishlist');
+$routes->get('/home/personal_page', 'Home::personal_page');
 $routes->setAutoRoute(true);
